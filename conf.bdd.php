4c47ac5c932a64fca939bbe1d36668bdf21fd0fe
Eva-30 <74191613+Eva-30@users.noreply.github.com>
2021-03-30 12:15:42 +0200

    Add files via upload

---
+++ conf.bdd.php
@@ -0,0 +1,8 @@
+<?php
+  // Création des constantes de connexions à la base de données
+  define("DB_SERVER",   "localhost");
+  define("DB_DATABASE", "country");
+  define("DB_USERNAME", "evab");
+  define("DB_PASSWORD", "paprika");
+
+?>
