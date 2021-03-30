2a5635beb3256b04665a419e517a2730178f9894
Eva-30 <74191613+Eva-30@users.noreply.github.com>
2021-03-30 12:20:32 +0200

    Add files via upload

---
+++ site/inc/inc.kickstart.php
@@ -0,0 +1,7 @@
+<?php
+
+  require("../conf.bdd.php"); // contient les constantes DB_SERVER, DB_DATABASE, DB_USERNAME, DB_PASSWORD
+  require("inc/inc.connexion.php"); // Instanciation de l'objet PDO(connexion base de données)
+  require("inc/inc.head.php"); //contient les métadonnées et la constante PAGE_TITLE "Bienvenue"
+
+?>
